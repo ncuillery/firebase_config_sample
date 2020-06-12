5d1b8c1327603841b9e962ae7e65ec0c09b3bedc
Nicolas Cuillery <nicolas.cuillery@gmail.com>
2020-06-12 15:07:31 -0400

    Init GA User Property in native

--- android/app/src/main/kotlin/com/cuillery/firebase_config_sample/MainActivity.kt
+++ android/app/src/main/kotlin/com/cuillery/firebase_config_sample/MainActivity.kt
@@ -1,6 +1,45 @@
 package com.cuillery.firebase_config_sample
 
+import android.os.Bundle
+import android.util.Log
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.analytics.ktx.logEvent
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 import io.flutter.embedding.android.FlutterActivity
 
+private lateinit var firebaseAnalytics: FirebaseAnalytics
+private lateinit var firebaseConfig: FirebaseRemoteConfig
+
 class MainActivity: FlutterActivity() {
+    private val TAG = "MainActivity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i(TAG, "onCreate")
+        firebaseAnalytics = Firebase.analytics
+        firebaseAnalytics.setUserProperty("app_country", "japan")
+        firebaseAnalytics.logEvent("app_created") {}
+
+        firebaseConfig = Firebase.remoteConfig
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 0
+        }
+        firebaseConfig.setConfigSettingsAsync(configSettings)
+
+        firebaseConfig.fetchAndActivate()
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val updated = task.result
+                        Log.i(TAG, "Fetch and activate succeeded: $updated")
+                        Log.i(TAG, firebaseConfig.getString("country"))
+                    } else {
+                        Log.i(TAG, "Fetch failed")
+                    }
+                }
+
+        super.onCreate(savedInstanceState)
+    }
+
 }
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,6 +10,28 @@ import Firebase
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
     FirebaseApp.configure()
+
+    Analytics.setUserProperty("japan", forName: "app_country")
+    Analytics.logEvent("app_created", parameters: nil)
+
+    let remoteConfig = RemoteConfig.remoteConfig()
+    let settings = RemoteConfigSettings()
+    settings.minimumFetchInterval = 0
+    remoteConfig.configSettings = settings
+
+    remoteConfig.fetch(withExpirationDuration: 0, completionHandler: { (status, error) -> Void in
+      if status == .success {
+        print("Config fetched!")
+        remoteConfig.activate() { (error) in
+          print("Config activated!")
+          print(remoteConfig.configValue(forKey: "country").stringValue!)
+        }
+      } else {
+        print("Config not fetched")
+        print("Error: \(error?.localizedDescription ?? "No error available.")")
+      }
+    })
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
